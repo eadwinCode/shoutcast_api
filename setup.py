0ae78f56c48a2c85a481862e16e5448e3d1485ee
eadwinCode <ezeudoh.tochukwu@gmail.com>
2020-03-03 06:40:30 +0100

    fixed endspoints returning a single station

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 
 setup(name='shoutcast-api',
-      version='1.0.2',
+      version='1.0.3',
       description='Shoutcast Radio Directory API ',
       author='eadwinCode',
       author_email='ezeudoh.tochukwu@gmail.com',
